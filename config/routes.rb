a718c00e747203a155ad048310ee202e0a6d82e8
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-14 17:26:59 +0800

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "pages#show" 
+
+  resources :pages
+  root "pages#index" 
+
+
+
+  namespace :admin do
+    resources :users, only: [:index, :destroy]
+    resources :templates 
+    resources :categories
+    root "users#index"
+  end  
 
 
 
