119d3486a4c1f729f854782582edb6cf6bac7ac6
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-15 11:13:51 +0800

    build basic spread api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
+
   def index
 
+    require 'google/apis/sheets_v4'
+    service = Google::Apis::SheetsV4::SheetsService.new
+    service.key = 'AIzaSyCw1eTY-S9Xuxqv4AZ_bfHDlxEJ3KsLuig'
+    service.authorization = nil
+    @response = service.get_spreadsheet_values("1u9ZBeEGHyGzORQAURnqvuWt03karagDSlc87geaTjMs", "abc!A2:A3")
  
-    
-
   end
-
-
 
 
 
