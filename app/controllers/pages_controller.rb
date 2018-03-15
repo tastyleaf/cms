ba8c6743e3fb7ac2b9efb5c6136cfcb315836f48
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-15 12:40:23 +0800

    basic api output

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,17 @@ class PagesController < ApplicationController
     service = Google::Apis::SheetsV4::SheetsService.new
     service.key = 'AIzaSyCw1eTY-S9Xuxqv4AZ_bfHDlxEJ3KsLuig'
     service.authorization = nil
-    @response = service.get_spreadsheet_values("1u9ZBeEGHyGzORQAURnqvuWt03karagDSlc87geaTjMs", "abc!A2:A3")
- 
+    response1 = service.get_spreadsheet_values("1m4xuNtoujwTySp3sXS2iZ0gsSk8m9d86S_NtReXHi7o", "nav-top!A2:A9")
+    @data1 = response1.values
+    response2 = service.get_spreadsheet_values("1m4xuNtoujwTySp3sXS2iZ0gsSk8m9d86S_NtReXHi7o", "promotion!A2:C2")
+    @data2 = response2.values
+    response3 = service.get_spreadsheet_values("1m4xuNtoujwTySp3sXS2iZ0gsSk8m9d86S_NtReXHi7o", "nav-middle!A2:A5")
+    @data3 = response3.values
+    response4 = service.get_spreadsheet_values("1m4xuNtoujwTySp3sXS2iZ0gsSk8m9d86S_NtReXHi7o", "item!B2:D9")
+    @data4 = response4.values
+    response5 = service.get_spreadsheet_values("1m4xuNtoujwTySp3sXS2iZ0gsSk8m9d86S_NtReXHi7o", "nav-bottom!B1:G8" )
+    @data5 = response5.values
+
   end
 
 
