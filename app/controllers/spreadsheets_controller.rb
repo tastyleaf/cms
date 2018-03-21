90bd1712fdd38507a6555caf14ea50776e61d52b
mordochi <lilynnchi@gmail.com>
2018-03-21 16:22:31 +0800

    make sure only user can see the backstage and install bootstrap

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -1,2 +1,27 @@
 class SpreadsheetsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def tabs
+
+  end
+
+  def range
+
+  end
+
+  def js
+
+  end
 end
