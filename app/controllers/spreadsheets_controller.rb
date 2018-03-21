8e8a44c73aa5f054ceabaa7701431a7d51c48585
Unknown <thetasteofleavesisgreat@gmail.com>
2018-03-21 21:27:56 +0800

    build basic tabs action

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -17,9 +17,19 @@ class SpreadsheetsController < ApplicationController
 
   def tabs
     set_spreadsheet
+
+    require 'google/apis/sheets_v4'
+    service = Google::Apis::SheetsV4::SheetsService.new
+    service.key = 'AIzaSyCw1eTY-S9Xuxqv4AZ_bfHDlxEJ3KsLuig'
+    service.authorization = nil
+    @reponses = service.get_spreadsheet(@spreadsheet.spreadsheet_name)
+    @tabs = service.get_spreadsheet(@spreadsheet.spreadsheet_name, fields: "sheets.properties").sheets
+
   end
 
-
+  def update
+    
+  end
 
 
 
